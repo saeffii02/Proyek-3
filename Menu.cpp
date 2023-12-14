952b343180b7b9935cd83851a364df1def083451
unknown <saeffii0225@gmail.com>
2023-12-14 16:24:01 +0700

    code-tambahdata

--- Menu.cpp
+++ Menu.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
 using namespace std;
 
+#define max 10
+string n[max], nik[max], kab[max], TTL[max], imunisasi[max], polio[max], campak[max], influenza[max], hepatitis_A[max], PCV[max],dengue[max], loop;
+int pos=0;
+
+void add(){
+	do{
+		if(pos<max){
+			cin.ignore();
+			cout<<"Nama : ";
+			getline(cin, n[pos]);
+			cout<<"nik : ";
+			getline(cin, nik[pos]);
+			cout<<"kab : ";
+			getline(cin, kab[pos]);
+			cout<<"Tempat, tanggal lahir : ";
+			getline(cin, TTL[pos]);
+			cout<<"usia 0-6 bulan "<<endl;
+			cout<<"  Imunisasi : "; 
+			getline(cin, imunisasi[pos]);
+			cout<<"  Polio : ";
+			getline(cin, polio[pos]);
+			cout<<"usia 6-12 bulan "<<endl;
+			cout<<"  Campak : "; 
+			getline(cin, campak[pos]);
+			cout<<"  Influenza : ";
+			getline(cin, influenza[pos]);
+			cout<<"usia 12-24 bulan"<<endl;
+			cout<<"  Hepatitis A : ";
+			getline(cin, hepatitis_A[pos]);
+			cout<<"  PCV : ";
+			getline(cin, PCV[pos]);
+			cout<<"usia 2-18 tahun "<<endl;
+			cout<<"  Campak : ";
+			getline(cin, campak[pos]);
+			cout<<"  Dengue : ";
+			getline(cin, dengue[pos]);
+			pos++;
+			cout<<"ulangi (y/t) : ";
+			cin>>loop;
+		}else{
+			cout<<"memori penuh"<<endl;
+			cout<<"isi t untuk kembali";
+			cin>>loop;
+		}
+	
+	}while(loop=="y");
+	cout<<"Tambahkan Data"<<endl;
+}
+
+
 int main(){
 	int pil;
 	do{
@@ -36,3 +86,4 @@ int main(){
 	cout<<"Program Selesai";
 	 
 }
+
